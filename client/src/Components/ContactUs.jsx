cdd7828c4cc8842c89da678b584ad3492a163f05
AbduLlaah Jumah <abdullaahjumah7@gmail.com>
2024-12-19 21:11:40 +0100

    Update

--- client/src/Components/ContactUs.jsx
+++ client/src/Components/ContactUs.jsx
@@ -7,7 +7,7 @@ function ContactUs() {
   return (
     <div className='small-pc:pad6 pad4 overflow-hidden w-full flex flex-col gap-[32.74px] items-center justify-center'>
         <div className="w-full h-[456.36px] tablet:h-[700px] phone:h-[780px] phone:py-8 overflow-hidden relative rounded-[24.56px] bg-gray-20 tablet:p-4">
-            <img alt='contact us bell' src={BellImg} className='absolute left-0 top-[-5px]' />
+            <img alt='contact us bell' src={BellImg} className='absolute left-0 top-[-5px] h-[300px]' />
             <div className='flex flex-col gap-[55.25px] w-[464.55px] tablet:w-full absolute tablet:flex tablet:mt-[12rem] tablet:p-4 top-[74px] right-[150px] tablet:left-4'>
                 <div className='flex flex-col gap-[24.56px]'>
                     <h2 className='text-gray-90 font-bold text-[36.84px]'>Contact Us</h2>
@@ -17,7 +17,7 @@ function ContactUs() {
                 </div>
 
                 <div>
-                    <div className='bg-[#0373DB] w-[50%] tablet:w-[70%] phone:w-[90%] flex items-center justify-center pt-[17.39px] pb-[17.39px] pl-[51.16px] pr-[51.16px] rounded-[10.23px]'>
+                    <div className='bg-[#0373DB] w-[35%] tablet:w-[70%] phone:w-[90%] flex items-center justify-center pt-[17.39px] pb-[17.39px] pl-[51.16px] pr-[51.16px] rounded-[10.23px]'>
                         <Link to='/contact' className='text-white'>
                             Contact Us
                         </Link>
